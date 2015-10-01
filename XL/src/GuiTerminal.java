250d319a0133a005c3213c5cee56feabe0f943b6
A JMac <AJ@As-MacBook-Pro.local>
2015-10-01 12:16:16 +0200

    La till GuiTerminal, endast i utvecklingsyfte

---
+++ XL/src/GuiTerminal.java
@@ -0,0 +1,17 @@
+import util.*;
+import model.*;
+import expr.*;
+
+public class GuiTerminal {
+	XLModel model;	
+	public static void main(String[] args) {
+		System.out.println("h");
+
+		model = new XLModel();
+			// Det här uppdatears nu.
+		model.addSlot("A1", "5");
+		slotMap.put("B2", "50");
+		
+	}
+
+}
